8ee8bd91ab7badf897471a4a74e699632d4b18eb
SazzadForazi <sazzad.csebubt@gmail.com>
2023-04-06 14:27:33 +0600

    contest1

---
+++ p4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    long long int b;
+    float c;
+    char d;
+    scanf("%d", &a);
+    scanf("%lld", &b);
+    scanf("%f", &c);
+    scanf("\n%c", &d);
+    printf("%d\n", a);
+    printf("%.2lld\n", b);
+    printf("%.2f\n", c);
+    printf("%c", d);
+    return 0;
+}
