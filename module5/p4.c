13e4dce5f0eebd93273808848836b1136fdc8a7f
SazzadForazi <sazzad.csebubt@gmail.com>
2023-04-09 01:15:35 +0600

    added

---
+++ module5/p4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+
+    int num;
+    scanf("%d", &num);
+    while (num >= 10)
+    {
+        num /= 10;
+    }
+    if (num % 2 == 0)
+    {
+        printf("EVEN\n");
+    }
+    else
+        printf("ODD\n");
+    return 0;
+}
