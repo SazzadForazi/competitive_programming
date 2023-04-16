76cc5bfa2c6a6c7a1f7aa05b1666e1ff63c7ddbb
SazzadForazi <sazzad.csebubt@gmail.com>
2023-04-16 20:34:59 +0600

    added

---
+++ module-9/copy.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int m;
+    scanf("%d", &m);
+    int b[m];
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    int res[m + n];
+
+    for (int i = 0; i < n; i++)
+    {
+        res[i] = a[i];
+    }
+    int i = n;
+    for (int j = 0; j < m; j++)
+    {
+        res[i] = b[j];
+        i++;
+    }
+
+    for (int i = 0; i < m + n; i++)
+    {
+        printf("%d ", res[i]);
+    }
+
+    return 0;
+}
