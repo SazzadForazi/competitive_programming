1ba901935054a57523622cd4e396bac1117a6f3e
SazzadForazi <sazzad.csebubt@gmail.com>
2023-05-11 21:13:48 +0600

    aded

---
+++ 2d_matrix/zero.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+
+    int row, col;
+    scanf("%d%d", &row, &col);
+    int a[row][col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int element = row * col;
+    int cnt = 0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (a[i][j] == 0)
+            {
+                cnt++;
+            }
+        }
+    }
+    if (element == cnt)
+    {
+        printf("Zero matrix");
+    }
+    else
+    {
+        printf("not zero");
+    }
+    return 0;
+}
